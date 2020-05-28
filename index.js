59c8d17f9624b8349828f58da4525f3520ce214a
Amanda Malamis <sitkoama@gmail.com>
2020-05-27 21:10:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('1')
+
+$(document).ready(function(e) {   
+    // your code here
+    $('.container-fluid').on( 'click', function() {
+        console.log('click')
+      });
+
+
+
+
+      
+ });
+
+
