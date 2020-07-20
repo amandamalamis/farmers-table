cbafcbba6dd6b726b3ffbbcd2ffc322d1ca92e59
Amanda Malamis <sitkoama@gmail.com>
2020-07-19 22:26:41 -0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-console.log('1')
+$(document).ready(function () {
+  $('.container-fluid').on('click', function () {
+    console.log('click')
+  });
 
-$(document).ready(function(e) {   
-    // your code here
-    $('.container-fluid').on( 'click', function() {
-        console.log('click')
-      });
+  $('.input-group-append button').on('click', function () {
+    if ($('.search-container').find('input')) {
+      var zipCodes = $('.search-container').find('input').val();
+      if ((zipCodes > 40000) && (zipCodes < 50000))
+        console.log('show deliver modal')
 
+    }
+    else (console.log('do not deliver'))
 
-
-
-      
- });
-
-
+  });
+});
